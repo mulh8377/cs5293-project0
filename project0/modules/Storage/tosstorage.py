a472756cb4080c2ba935832b7456a696f2e11fe1
mulh8377 <vontossman@gmail.com>
2020-02-18 18:40:42 -0600

    more stuff

--- project0/modules/Storage/tosstorage.py
+++ project0/modules/Storage/tosstorage.py
@@ -1,2 +1,32 @@
+import sqlite3
+import os
+
+def create_db():
+    os.system("bash /home/mulh8377/Courses/TextAnalyis/projects/cs5293-project0/scripts/jah-db.sh")
+def kill_db():
+    os.system("bash /home/mulh8377/Courses/TextAnalyis/projects/cs5293-project0/scripts/clean-db.sh")
+def connect_to_db():
+    return sqlite3.connect('../../../normanpd.db')
+
+def insert_into_crime_report():
+    connection = connect_to_db()
+    usr = connection.cursor()
+
+    connection.commit()
+
+    connection.close() ## close connection
+
+def read_from_crime_report():
+    connection = connect_to_db()
+    usr = connection.cursor()
+    if connection:
+        print(connection)
+    connection.close()
+
 if __name__ == "__main__":
-    print(f"tosstorage.py is designed to handle database reading, manipulation, and transformation")
+    #print(f"tosstorage.py is designed to handle database reading, manipulation, and transformation")
+    #insert_into_crime_report()
+
+    #read_from_crime_report()
+    #kill_db()
+    create_db()
